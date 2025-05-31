b3c223c4224b36924fb6666658f55d63327dc55e
ArthurBlasi <rthrblasi@gmail.com>
2025-05-31 04:13:02 -0300

    taking trash out of the popup file

--- feedback-extension/src/popup/Popup.tsx
+++ feedback-extension/src/popup/Popup.tsx
@@ -10,24 +10,6 @@ import NavigateButton from "../components/NavigateButton/NavigateButton";
 
 const Popup = () => {
 
-  const [message, setMessage] = useState("");
-
-  const handleSend = () => {
-    if (!message.trim()) {
-      alert("Mensagem vazia!");
-      return;
-    }
-
-    // Aqui você poderia enviar o feedback via API
-    console.log("Enviado:", message);
-    alert("Feedback enviado com sucesso!");
-    setMessage(""); // limpa após enviar
-  };
-
-  const handleClear = () => {
-    setMessage("");
-  };
-
   const handleIncreaseView = () => {
      chrome.tabs.create({ url: chrome.runtime.getURL("webview.html") });
   };
